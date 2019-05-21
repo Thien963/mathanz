f1d9d46a755b0135b15e3be95e930634d2d1f129
ADMIN <ngominhthien_t62@hus.edu.vn>
2019-05-21 19:43:31 +0700

    21052019

--- index.php
+++ index.php
@@ -3,10 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-
-
 </head>
 <body>
+
+<?php
+$weight = $height = '';
+if (isset($_POST["height"]) && isset($_POST["weight"])) {
+
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
+    $weight = $_POST["weight"];
+    $height = $_POST["height"];
+
+    $bmi = $weight/($height*$height);
+
+    echo "Chỉ sô BMI là : " . $bmi;
+}
+?>
 
 <div class="container">
     <div class="row">
@@ -16,17 +30,20 @@
             <form name="bmi" action="" method="post">
                 <div class="form-group">
                     <label >Chiều cao ( cm )</label>
-                    <input type="text" class="form-control" name="height" value="">
+                    <input type="text" class="form-control" name="height" value="<?php echo $height ?>">
                 </div>
                 <div class="form-group">
                     <label >Cân nặng ( kg ) </label>
-                    <input type="text" class="form-control" name="weight">
+                    <input type="text" class="form-control" name="weight" value="<?php echo $weight ?>">
                 </div>
                 <button type="submit" name="calc" class="btn btn-primary">Submit</button>
             </form>
         </div>
     </div>
 </div>
+
+
+
 
 </body>
 </html>
